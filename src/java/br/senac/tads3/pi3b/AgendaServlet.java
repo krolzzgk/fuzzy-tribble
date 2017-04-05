cd38ed41962b2dd1d592ab12e56da375e5ab1b55
karolina.gkallajian <karolina.gkallajian@CAS511780W0726.senacsp.edu.br>
2017-04-04 21:29:06 -0300

    Merge branch01
    Conflicts:
    	testegit/src/main/java/br/senac/tads/pi3/testegit/Principal.java

---
+++ src/java/br/senac/tads3/pi3b/AgendaServlet.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads3.pi3b;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author karolina.gkallajian
+ */
+@WebServlet("/agenda")
+public class AgendaServlet extends HttpServlet{
+    
+    /**
+     *
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        response.setContentType("text/html");
+        try(PrintWriter out = response.getWriter()){
+            for (int i = 0; i < 10; i++) {
+                
+                out.print("<h1>*~*~*Olá mundo Web*~*~*</h1>");
+                out.print("<h1>*~*~*~*~*~*~*~*~*~*~*~*</h1>");
+                
+            }
+            out.flush();
+        }catch(IOException e){
+            
+        }
+    }
+    
+}
---
+++ web/agenda2.jsp
@@ -0,0 +1,25 @@
+<%-- 
+    Document   : agenda2
+    Created on : 04/04/2017, 21:20:44
+    Author     : karolina.gkallajian
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>*~*~*~*~*~*~*~*~*~*~*~*</h1>
+        <%
+            for (int i = 0; i < 10; i++) {
+        %>
+        <h1>*~*~*Olá mundo Web*~*~*</h1>
+        <h1>*~*~*~*~*~*~*~*~*~*~*~*</h1>
+        <%
+            }
+        %> 
+    </body>
+</html>
